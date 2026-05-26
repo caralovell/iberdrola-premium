dddd9ab44beaa2614fc63ffeae746cd6547064f0
caralovell <168685711+caralovell@users.noreply.github.com>
2026-05-26 12:56:11 +0200

    Update TecnologiasSection.tsx

--- src/components/TecnologiasSection.tsx
+++ src/components/TecnologiasSection.tsx
@@ -4,7 +4,7 @@ import ScrollReveal from "./ScrollReveal";
 // Imágenes ya disponibles en tu proyecto
 import vistaTienda from "@/assets/gestor.png";
 import mockupCuestionario from "@/assets/mockup-cuestionario.png";
-import mockupRecomendacion from "@/assets/mockup-recomendacion.png";
+import mockupRecomendacion from "@/assets/asistente.png";
 import wipass from "@/assets/iberdrolanfc.png";
 
 const tecnologias = [
