9ba5a1bb8af0aa48e78e576eaec87868a07841d6
caralovell <168685711+caralovell@users.noreply.github.com>
2026-05-29 11:41:06 +0200

    Update RecorridoSection.tsx

--- src/components/RecorridoSection.tsx
+++ src/components/RecorridoSection.tsx
@@ -22,7 +22,7 @@ import {
 } from "lucide-react";
 import ScrollReveal from "./ScrollReveal";
 import pre from "@/assets/pre-entrada.png";
-import entrada from "@/assets/dentro.png";
+import entrada from "@/assets/pisando.png";
 import bienvenida from "@/assets/bienvenida.png";
 import dina from "@/assets/dinamizacion.png";
 import entorno from "@/assets/display.png";
